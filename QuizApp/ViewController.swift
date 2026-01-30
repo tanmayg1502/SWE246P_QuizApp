59555109d349360a641504152c1f288febbd76e7
Tanmay Goel <tanmaygoel1502@gmail.com>
2026-01-29 16:44:32 -0800

    Excercise 1

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet var questionLabel: UILabel!
+	@IBOutlet var answerLabel: UILabel!
+	
+	let questions: [String] = [
+		"What is DRS",
+		"Who won WDC more than 5 times",
+		"Even if you are not fan of this team, you are a fan of this team"
+	]
+	
+	let answer: [String] = [
+		"Drag Reduction System",
+		"Lewis Hamilton",
+		"Ferrari"
+	]
+	
+	var currentQuestionIndex: Int = 0
+	
+	@IBAction func showNextQuestion(_ sender: Any) {
+		currentQuestionIndex += 1
+		if currentQuestionIndex == questions.count {
+			currentQuestionIndex = 0
+		}
+		
+		let question: String = questions[currentQuestionIndex]
+		questionLabel.text = question
+		answerLabel.text = "???"
+	}
+	
+	@IBAction func showAnswer(_ sender: Any) {
+		let answer: String = answer[currentQuestionIndex]
+		answerLabel.text = answer
+		
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		questionLabel.textAlignment = .center
+		questionLabel.numberOfLines = 0
+		questionLabel.text = questions[currentQuestionIndex]
 	}
 
 
